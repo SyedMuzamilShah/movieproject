90351dfab46effdcb8e0eb3e46346340f12ccd96
2023HITMAN <syed.m.shah8989@gmail.com>
2023-04-20 11:23:45 -0700

    change is possible

--- index.js
+++ index.js
@@ -1,62 +1,54 @@
-let loginForm = document.getElementById("loginForm");
-loginForm.addEventListener("submit", (e) => {
+let searchForm = document.getElementById("SearchForm");
+searchForm.addEventListener("submit", async (e) => {
     e.preventDefault();
-    const gener = document.querySelector('#genre');
-    const year = document.querySelector('#year');
-    const language = document.querySelector('#language');
-    const ranting = document.querySelector('#ranting');
+    const generValue = document.querySelector('#MovieType').value;
+    const yearValue = document.querySelector('#Year').value;
+    const languageValue = document.querySelector('#Language').value;
+    const rantingValue = document.querySelector('#Ranting').value;
 
-    var GenerValue = gener.options[gener.selectedIndex].value;
-    var YearValue = year.options[year.selectedIndex].value;
-    var LanguageValue = language.options[language.selectedIndex].value;
-    var RantingValue = ranting.options[ranting.selectedIndex].value;
+    let response = await fetch('data.json');
+    let data = await response.json();
+    let items = [];
 
+    for (let index in data) {
+        const type = data[index].genres;
+        const language = data[index].original_language;
+        const release = data[index].release_date.slice(0, 4);
+        if (release === yearValue) {
+            items.push(data[index]);
+        }
+        if (language === languageValue) {
+            items.push(data[index]);
+        }
+        if (type[0] === generValue) {
+            items.push(data[index]);
+        }
+    }
+    
+    var SreachItemsPlace = document.getElementById('placesreachitems')
+    var next = document.getElementById('place')
+    next.innerHTML = '';
+    items.forEach((item, index) => {
+        const { runtime, poster_path, title, certification, genres, homepage } = item;
+        var copy = SreachItemsPlace.cloneNode(true);
+        copy.querySelector('#rankid').innerText = index + 1;
+        copy.querySelector('#imageid').src = `https://image.tmdb.org/t/p/w500${poster_path}`;
+        copy.querySelector('#titleid').textContent = title;
+        copy.querySelector('#certificateid').textContent = certification;
+        copy.querySelector('#movietypeid').textContent = genres
+        copy.querySelector('#playtimeid').textContent = timecal({ runtime });
+        copy.querySelector('#likeid').href = homepage;
+        
+        next.appendChild(copy);
+    });
+    items = []
+    }
+);
 
-    const data = fetch('data.json')
-        .then(response => response.json())
-        .then((json) => {
-            var item = []
-            for (var g in json) {
-                const type = json[g].genres
-                const language = json[g].original_language
-                const release = json[g].release_date.slice(0, 4)
-                if (release === YearValue || language === LanguageValue || type[0] === GenerValue) {
-                    item.push(json[g])
-                }
-            }
-            for (let put = 0; put < item.length; put++) {
-                for (var a in item) {
-                    PutTitle.innerText = item[a].title
-                    PutType.innerText = item[a].certification
-                    PutMovieType.innerText = json[0].genres.join(', ')
-                    PutYear.innerText = item[a].release_date.slice(0, 4)
-                    PutMoviePlayTime.innerHTML = timecal()
-                    PutImage.src = `https://image.tmdb.org/t/p/w500${item[a].poster_path}`
-                    
-                    function timecal() {
-                        const totalMinutes = item[a].runtime;
-                        const hours = Math.floor(totalMinutes / 60); // 1
-                        const minutes = totalMinutes % 60; // 46
-                        const formattedTime = `${hours}h ${minutes}min`;
-                        return formattedTime;
-                    }
-
-                }
-            }
-
-
-        })
-});
-
-
-// puting place gatting
-var PutTitle = document.getElementById('title')
-var PutImage = document.getElementById('image')
-var PutType = document.getElementById('type')
-var PutMovieType = document.getElementById('movietype')
-var PutYear = document.getElementById('YEAR')
-var PutMoviePlayTime = document.getElementById('movieplaytime')
-
-
-
-var PuttingDataInTable = document.getElementById("PutData");
+function timecal(cal) {
+    const totalMinutes = cal.runtime;
+    const hours = Math.floor(totalMinutes / 60); // 1
+    const minutes = totalMinutes % 60; // 46
+    const formattedTime = `${hours}h ${minutes}min`;
+    return formattedTime;
+}
